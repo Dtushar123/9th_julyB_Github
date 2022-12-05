5fbf1ddfe60d9d054a7fd88d5ba2abdd9714a96e
HRUSHIKESH DHAYAGUDE <HRUSHIKESH DHAYAGUDE@MSI>
2022-12-05 08:28:29 +0530

    Tushar:demo

---
+++ src/sample/demo.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class demo 
+{
+
+	public static void main(String[] args) 
+	{
+	System.out.println("hi "
+			+ "username-Dtushar123\r\n"
+			+ "\r\n"
+			+ "password-Tushar_99    // token->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\r\n"
+			+ "\r\n"
+			+ "url-https://github.com/Dtushar123/9th_julyB_Github.git");	
+	}
+}
